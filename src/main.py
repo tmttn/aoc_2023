d570202364346babd74a0af9c4a753fc19182a13
Thomas Metten <tom.metten@acagroup.be>
2023-12-02 21:38:30 +0100

    feat: run a day and part from the command-line

--- src/main.py
+++ src/main.py
@@ -1,5 +1,50 @@
-# from day_01 import part_1, part_2
-from day_02 import part_1, part_2
+"""
+This is the main script for the Advent of Code 2023 project.
+It allows the user to run the solution for a specific day and part.
+"""
+import argparse
+import importlib
+import os
+import sys
 
-# part_1.solve()
-part_2.solve()
+# Parse the command line arguments
+parser = argparse.ArgumentParser(description="Advent of Code 2023")
+parser.add_argument(
+    "day",
+    metavar="day",
+    type=int,
+    help="the day of the challenge (1-25)",
+)
+parser.add_argument(
+    "part",
+    metavar="part",
+    type=int,
+    help="the part of the challenge (1 or 2)",
+)
+parser.add_argument(
+    "--input",
+    metavar="input",
+    type=str,
+    help="the path to the input file",
+)
+args = parser.parse_args()
+
+# Check if the input file exists
+if args.input and not os.path.isfile(args.input):
+    print(f"Input file {args.input} does not exist.")
+    sys.exit(1)
+    
+# Import the module for the given day and part
+module_name = f"day_{args.day:02d}.part_{args.part}"
+try:
+    module = importlib.import_module(module_name)
+except ModuleNotFoundError:
+    print(f"Module {module_name} not found.")
+    sys.exit(1)
+    
+# Solve the puzzle
+try:
+    module.solve()
+except AttributeError:
+    print(f"Function solve() not found in module {module_name}.")
+    sys.exit(1)
