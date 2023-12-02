1577c05e2a7b1dbd7d7c3d0babb43427c55e6703
Thomas Metten <tom.metten@acagroup.be>
2023-12-02 21:47:28 +0100

    fix: correct doc text

--- src/common/file_reader.py
+++ src/common/file_reader.py
@@ -1,20 +1,15 @@
-"""_summary_
-
-    Returns:
-        _type_: _description_
-    """
+"""This module contains the function for reading the input file for the given module.
+"""
 import os
 
-
 def read_input_file(file):
-    """
-    Reads the contents of the input file.
-
-    Args:
-        file (str): The path to the input file.
-
+    """Reads the input file for the given module.
+    
+    Parameters:
+    file (str): The path to the module.
+    
     Returns:
-        str: The contents of the input file as a string.
+    list: A list of strings representing the lines from the input file.
     """
     # Change the working directory
     os.chdir(os.path.dirname(os.path.abspath(file)))
